5f82b1f879c44ec964c4c3a91bf137b0479e5fe5
petro438 <stevenjpetrella@gmail.com>
2025-06-26 16:31:38 -0400

    Update server.js

--- cfb-api/server.js
+++ cfb-api/server.js
@@ -2146,6 +2146,14 @@ app.get('/', (req, res) => {
   });
 });
 
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch all handler: send back React's index.html file for any non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`CFB API server running on port ${port}`);
   console.log(`Database: ${process.env.DB_NAME || process.env.DB_DATABASE}`);

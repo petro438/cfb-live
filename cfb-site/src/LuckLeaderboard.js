86d804c7dd2c569c06fba2c1eb533c2557dfc5fd
petro438 <stevenjpetrella@gmail.com>
2025-06-30 00:17:37 -0400

    Update LuckLeaderboard.js

--- cfb-site/src/LuckLeaderboard.js
+++ cfb-site/src/LuckLeaderboard.js
@@ -17,7 +17,7 @@ const LuckLeaderboard = () => {
     try {
       setLoading(true);
       
-      const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+      const API_URL = 'https://cfb-api-production.up.railway.app';
       
       const params = new URLSearchParams({
         includePostseason: (!regularSeasonOnly).toString(),
